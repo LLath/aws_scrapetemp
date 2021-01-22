ee6c47b41ba258e11b6bf697a79c5ebad6fb5316
LLath <rothpatrick12@yahoo.de>
2021-01-22 06:09:15 +0100

    fix: promise to async so connection loss doenst happen

--- handler.js
+++ handler.js
@@ -1,7 +1,16 @@
 "use strict";
+const util = require("util");
 const { scrapeArticles } = require("./scrapeArticles");
 const AWS = require("aws-sdk");
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const safePromisify = function (fun, methodsArray) {
+  const suffix = "Async";
+  methodsArray.forEach((method) => {
+    fun[method + suffix] = util.promisify(fun[method]);
+  });
+};
+safePromisify(dynamoDb, ["put"]);
 
 const TABLE_NAME = process.env.DYNAMODB_TABLE;
 
@@ -28,6 +37,11 @@ const getLastItem = () => {
   return dynamoDb.query(params).promise();
 };
 
+const putItems = async (params) => {
+  console.log("Put Item");
+  await dynamoDb.putAsync(params);
+};
+
 module.exports.scrapeTemp = async (event) => {
   console.log("Start scraping");
   const resArr = [];
@@ -39,50 +53,45 @@ module.exports.scrapeTemp = async (event) => {
     .then(() => {
       console.log("Scan for last Item");
       return getLastItem().then(({ Items }) => {
-        let inFunction = true;
         const sortResArr = resArr.sort(
           (a, b) => new Date(a.time) - new Date(b.time)
         );
-        let newItems = sortResArr;
+        let newItems;
         if (Items.length >= 1) {
-          const times = sortResArr.map((v) => v.time);
-          const indexItem = times.indexOf(Items[0].time);
+          const urls = sortResArr.map((v) => v.url);
+          const indexItem = urls.indexOf(Items[0].url);
           newItems = sortResArr.slice(indexItem + 1);
+        } else {
+          newItems = sortResArr;
         }
-        if (newItems.length > 0) {
-          console.log("Found new Item");
-          let prevTime;
-          newItems.forEach((data, index) => {
-            if (prevTime === data.time) {
-              data.time += 1;
-            }
-            prevTime = data.time;
-
-            const params = {
-              TableName: TABLE_NAME,
-              Item: data,
-            };
-            dynamoDb.put(params, (err, data) => {
-              console.log("Put Item");
-              if (err) console.error(err);
-              else console.log("End Putting");
-            });
-            if (index === newItems.length) inFunction = false;
-          });
-          // End forEach
-        } else console.log("No new Items found");
-        return inFunction;
+        return newItems;
       });
     })
-    .then((v) => {
-      if (!v) {
-        console.log("End Scraping");
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: "Success!",
-          }),
-        };
-      }
+    .then(async (items) => {
+      if (items.length > 0) {
+        console.log("Found new Item");
+        let prevTime;
+        items.map(async (data) => {
+          if (prevTime === data.time) {
+            data.time += 1;
+          }
+          prevTime = data.time;
+          const params = {
+            TableName: TABLE_NAME,
+            Item: data,
+          };
+          await putItems(params);
+        });
+        // End forEach
+      } else console.log("No new Items found");
+    })
+    .then(() => {
+      console.log("End Scraping");
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Success!",
+        }),
+      };
     });
 };
