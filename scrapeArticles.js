6284e7cfb3cb7c95ce46a3fdad6210e93a59f72d
LLath <rothpatrick12@yahoo.de>
2021-05-25 18:13:54 +0200

    chore: switch to cheerios because node-html-parser has a problem with not closed li

--- scrapeArticles.js
+++ scrapeArticles.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
-const { parse } = require("node-html-parser");
 const Entities = require("html-entities").AllHtmlEntities;
+const { load } = require("cheerio");
 
 const entities = new Entities();
 
@@ -12,35 +12,44 @@ const scrapeArticles = (URL) => {
   return new Promise((resolve, reject) =>
     fetch(URL)
       .then((res) => res.text())
-      .then((text) => parse(text))
-      .then((_html) =>
-        _html.querySelector(".page-content")
-          ? _html.querySelector(".page-content")
-          : _html
-      )
-      .then((content) => {
-        const articles = content.querySelectorAll("article");
-        const result = articles.map((article) => ({
-          title: entities.decode(article.querySelector(".entry-title").rawText),
-          url: decodeURI(
-            article
-              .querySelector(".entry-title")
-              .querySelector("a")
-              .rawAttrs.match(regexURL)[0]
-          ),
-          description: article.querySelector("p")
-            ? entities.decode(article.querySelector("p").structuredText)
-            : "",
-          time: article
-            .querySelector(".entry-meta")
-            .querySelector("time")
-            .rawAttrs.match(regexTime)[0],
-          type: URL.split("/")[URL.split("/").length - 2].replace("-en", ""),
-          master: "master",
-        }));
-        resolve(result);
+      .then((text) => {
+        const list = [];
+        const $ = load(text);
+        $(".wp-block-latest-posts__list")
+          .find("li")
+          .each((index, post) => {
+            list.push({
+              title: entities.decode($(post).find("a").text()),
+              url: decodeURI($(post).find("a").attr("href")),
+              description: "",
+              time: $(post).find("time").attr("datetime"),
+              type: URL.split("/")[URL.split("/").length - 2].replace(
+                "-en",
+                ""
+              ),
+              master: "master",
+            });
+          });
+        resolve(list);
       })
   );
 };
+
+const URLARR = [
+  "https://kr-official.community/en-community/notice-en/",
+  "https://kr-official.community/en-community/patchNote-en/",
+  "https://kr-official.community/en-community/event-en/",
+  "https://kr-official.community/en-community/specialShop-en/",
+  "https://kr-official.community/en-community/gameContent-en/",
+  "https://kr-official.community/en-community/guide/",
+  "https://kr-official.community/en-community/kings-raid-media/",
+];
+
+// scrapeArticles(URLARR[1]).then((v) => console.log(v));
+
+// URLARR.map((a) => {
+//   console.log(a);
+//   scrapeArticles(a).then(() => console.log("Fertig"));
+// });
 
 module.exports = { scrapeArticles };
